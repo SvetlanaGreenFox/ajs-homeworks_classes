0bab63469c74cc6f05b024db1a552828fc08d515
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-10-06 23:02:55 +0300

    Make tests

---
+++ src/js/__tests__/method.test.js
@@ -0,0 +1,6 @@
+import Bowerman from "../__classes__/bowerman";
+import Daemon from "../__classes__/daemon";
+import Magician from "../__classes__/magician";
+import Swordsman from "../__classes__/swordsman";
+import Undead from "../__classes__/undead";
+import Zombie from "../__classes__/zombie";
