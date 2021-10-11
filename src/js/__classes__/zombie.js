4e622771f24ed05ce8b3eb6b238b349cdca2c76d
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-10-11 17:03:56 +0300

    Corrected value

--- src/js/__classes__/zombie.js
+++ src/js/__classes__/zombie.js
@@ -3,7 +3,7 @@ import Character from './character';
 export default class Zombie extends Character {
   constructor(name, type = 'Zombie') {
     super(name, type);
-    this.attack = 25;
-    this.defence = 25;
+    this.attack = 40;
+    this.defence = 10;
   }
 }
